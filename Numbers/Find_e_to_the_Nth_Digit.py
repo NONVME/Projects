1c416f8a1fadd21d632a3795e7564abaab7b7e48
Chashin Efim <69835249+NONVME@users.noreply.github.com>
2020-12-06 10:41:13 +0000

    add: Find e to the Nth Digit

---
+++ Numbers/Find_e_to_the_Nth_Digit.py
@@ -0,0 +1,25 @@
+from decimal import Decimal
+from math import e as constant
+
+MAX_LIMIT = len(str(Decimal(constant))) - 1
+
+
+def limit_decimal(num, limit):
+    return f'The meaning of e: {num:.{limit}f}'
+
+
+while True:
+    try:
+        limit = (int(input(
+            "Please, enter the maximum number of decimal places for e: ")))
+        if limit >= MAX_LIMIT:
+            print(f'Enter a number smaller than {MAX_LIMIT}.')
+            continue
+        elif limit < 0:
+            print('Please, enter nonnegative value')
+            continue
+    except ValueError:
+        print('Please, enter a number')
+    else:
+        print(limit_decimal(constant, limit))
+        break
